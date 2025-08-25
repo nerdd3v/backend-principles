0ca41022514abaa3e11e4951e86493164877fa5e
n3rddev <n3rddev.code@gmail.com>
2025-08-25 13:08:38 +0530

    logout functionality

--- Cookie-Auth-FE/src/UserComp.tsx
+++ Cookie-Auth-FE/src/UserComp.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 //@ts-ignore
 function UserComp(props) {
@@ -7,7 +8,18 @@ function UserComp(props) {
         <h1>hdlliopvc</h1>
       <h1>{props.email}</h1>
       <button onClick={async()=>{
-        
+        try {
+            const response = axios.post('http://localhost:3000/logout',{
+            },{
+                withCredentials:true
+            }
+        )
+        if((await response).data.message === 'logged out'){
+            alert('hahah')
+        }
+        } catch (error) {
+            alert("Logout failed");
+        }
       }}>logout</button>
     </div>
   )
